fcceb97a094c4a230e29deef196b73e4c4c39346
Kavin M R <154052561+Kavinmr62@users.noreply.github.com>
2025-05-28 07:05:22 +0530

    Update and rename fibonacci series using function.txt to fibonacci series.java

---
+++ fibonacci series.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main{
+public static void main(String[] args){
+    Scanner in=new Scanner(System.in);
+    int n=in.nextInt();
+    int a=0;
+    int b=1;
+    System.out.print(a+","+b);
+    for(int i=2;i<n;i++)
+    {
+     int c=a+b;
+     System.out.print(","+c);
+     a=b;
+     b=c;
+    }
+}}
